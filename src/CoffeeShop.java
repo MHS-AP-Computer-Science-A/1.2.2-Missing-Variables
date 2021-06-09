cff966f56a157e1bc47a41ed607f8823a1507f70
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-06-09 10:40:30 -0400

    Add files via upload

---
+++ src/CoffeeShop.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class CoffeeShop {
+
+	public static void main(String[] args) {
+
+		// Declare your variables here
+
+		
+		
+		
+		
+		
+		// Do not alter code below this line
+
+		// Get the name on the order
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the name on the order?");
+		orderName = input.next();
+
+		// Get the items for the order and compute the subtotal
+		while (orderFinished == false) {
+			System.out.println("What item would you like?");
+			itemName = input.next();
+			if (itemName.equals("Coffee")) {
+				total = total + 2.50;
+				numItems++;
+			} else if (itemName.equals("Latte")) {
+				total = total + 5.49;
+				numItems++;
+			} else if (itemName.equals("Smoothie")) {
+				total = total + 4.25;
+				numItems++;
+			} else {
+				orderFinished = true;
+			}
+		}
+
+		// Add the tax to the total
+		total = total * TAXRATE;
+
+		// Display the order
+		System.out.println("Order for " + orderName);
+		System.out.println(numItems + " items");
+		System.out.println("Amount due: $" + total);
+
+	}
+
+}
